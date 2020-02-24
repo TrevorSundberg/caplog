84bc6703b9c7d5fddfd715c60977d31449fda7a7
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-02-24 09:22:00 -0800

    Change the build container to muxd/build

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 REPO=`git rev-parse --show-toplevel`
-docker build -t muxd $REPO/docker
+docker build -t muxd/build $REPO/docker
 docker run \
     --rm \
     -u $(id -u):$(id -g) \
     -e CCACHE_DIR="$REPO/build/cache" \
     -v "$REPO:$REPO" \
     -w "`pwd`" \
-    muxd \
+    muxd/build \
     "$@"
