1075282717712c80679ff9f00c1344d2ed04c85e
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-02-25 08:52:10 -0800

    Fix for permissions with the tests

--- test/run.sh
+++ test/run.sh
@@ -3,4 +3,4 @@ set -e
 cd `dirname "$0"`
 ../build.sh
 docker build -t muxd/test -f Dockerfile ..
-docker run --rm -v /usr/share/zoneinfo/UTC:/usr/share/zoneinfo/UTC muxd/test
+docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --rm -v /usr/share/zoneinfo/UTC:/usr/share/zoneinfo/UTC muxd/test
